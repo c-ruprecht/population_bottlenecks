c6452bfc4d5ed507fc54ba80090a0d7b7e37c83d
c-ruprecht <constantin.ruprecht@gmail.com>
2025-10-16 09:56:27 +0200

    first commit

---
+++ Snakefiles/run_snake9_batch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Setup
+
+# Default Snakefile
+SNAKEFILE="Snakefiles/ampliconez_Snakefile"
+
+# Parse command-line arguments
+while getopts "s:" opt; do
+  case $opt in
+    s)
+      SNAKEFILE="$OPTARG"
+      ;;
+    *)
+      echo "Usage: $0 [-s snakefile_path]"
+      exit 1
+      ;;
+  esac
+done
+
+# remove clustger log files
+rm -rf /sc/arion/work/ruprec01/log/cluster/*
+mkdir -p /sc/arion/work/ruprec01/log/cluster
+
+
+#Define inout and output directories
+INPUT_DIRS=('/sc/arion/projects/faithj02a/data/seq/2025/ILL138',
+            '/sc/arion/projects/faithj02a/data/seq/2025/ILL135',
+            '/sc/arion/projects/faithj02a/data/seq/2025/ILL134')
+            #'/sc/arion/projects/faithj02a/data/seq/2025/ILL132/30-1179742904/00_fastq',
+            # '/sc/arion/projects/faithj02a/data/seq/2025/ILL130/00_fastq',
+OUTPUT_DIRS=('/sc/arion/work/ruprec02/population-bottlenecks/ILL138',
+             '/sc/arion/work/ruprec02/population-bottlenecks/ILL135',
+             '/sc/arion/work/ruprec02/population-bottlenecks/ILL134')
+             #'/sc/arion/work/ruprec02/population-bottlenecks/ILL132',
+             #'/sc/arion/work/ruprec02/population-bottlenecks/ILL130',
+
+
+# Run with Snakemake 9.1.1
+# rerung specific rules -R get_single_GD get_GD_aggregate
+for i in "${!INPUT_DIRS[@]}"; do
+  snakemake \
+    -s ${SNAKEFILE} \
+    --config input_dir="${INPUT_DIRS[$i]}" output_dir="${OUTPUT_DIRS[$i]}" \
+    --jobs 500 \
+    --cores 1 \
+    --rerun-incomplete \
+    --latency-wait 400000 \
+    --executor lsf \
+    --default-resources \
+      mem_mb=16000 \
+      disk_mb=10000 \
+      lsf_project="acc_faithj02a" \
+      lsf_queue="express" \
+      walltime=120 \
+      "lsf_extra='-o /sc/arion/work/ruprec01/log/cluster/%J.out -e /sc/arion/work/ruprec01/log/cluster/%J.err -L /bin/bash'"
