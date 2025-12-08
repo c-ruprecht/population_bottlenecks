2a86074057085950a23b1f471d28242d979a23b3
c-ruprecht <constantin.ruprecht@gmail.com>
2025-12-08 14:55:01 +0100

    update metadata files

--- scripts/create_experiment_moleculetables.py
+++ scripts/create_experiment_moleculetables.py
@@ -21,6 +21,179 @@ def main():
     df_empty = df_empty[['ILL', 'sample', 'is_empty']].copy()
     df_empty.columns = ['ill', 'file_name','is_empty']
     df_empty['ill'] = 'ILL' + df_empty['ill'].astype(str)
+    ### P4C3T1
+    print(50* '=')
+    print(' Creating moleculetables for P4C3T2 100K barcodese')
+    print(50*'-')
+    print(' ')
+    try:
+        basepath = Path(args.basepath)
+        exportpath = Path(args.output) / 'P4C3T1/moleculetables'
+        exportpath.mkdir(parents=True, exist_ok=True)
+
+        ## Read metadata & ill sheets
+        metadir = Path(args.metadir)
+        #P4C2T4:
+        df_ill = pd.read_excel(metadir / 'ill_sheets' / 'ILL134_sample_sheet.xlsx',
+                                    sheet_name = 'Individual Library Information', header = 1)
+        df_samplesheet = pd.read_csv(metadir / 'metadata' / 'P4C3T1_cr_mouse_sample - P4C3T1.tsv', sep = '\t')
+
+        df_samplesheet = df_samplesheet.loc[df_samplesheet['sample_type'].str.contains('DNA')].copy()
+        df_ill = df_ill.dropna(subset=['Sample Name/Pool Name*'])
+        df_ill['sample_id']= df_ill['Sample Name/Pool Name*'].apply(lambda x: str(x).split('_')[0])
+        df_ill['project'] = df_ill['Library Name*'].apply(lambda x: str(x).split('_')[1])
+        #correct library name in ILL138 run
+        df_ill['ill'] = df_ill['Library Name*'].apply(lambda x:  str(x).split('_')[0])
+        df_ill['file_name'] = df_ill['Sample Name/Pool Name*'].apply(lambda x: str(x).replace('_', '-'))
+        df_merge = pd.merge(df_ill[['sample_id', 'ill', 'file_name', 'project']], df_samplesheet, how = 'right', on = ['sample_id', 'project'])
+        df_merge.head(10)
+
+        df_ill_ntc = df_ill.loc[df_ill['Sample Name/Pool Name*'].str.contains("WATER|NTC")]
+        df_merge = df_merge.dropna(subset= ['file_name'])
+        print(df_merge)
+        print(df_ill_ntc)
+
+        
+        li_strains = ['ST1', 'ST2']
+        df_ntc = pd.DataFrame()
+        for idx, row in df_ill_ntc.iterrows():
+            file_path = basepath / row['ill'] / 'readtables' / row['file_name'] / f"{row['file_name']}_total_moleculestable.csv"
+            df = pd.read_csv(file_path)
+            df['sample'] = row['Sample Name/Pool Name*']+'_'+row['sample_id']
+            df_ntc = pd.concat([df_ntc, df])
+        
+        df_ntc_pivot = df_ntc.pivot(index = ['strain', 'umi_seq'], columns = 'sample', values = 'molecules').fillna(0)
+        print('FOUND GAVAGE DOSES:')
+        print(df_merge['gavage_dose_200ul'].unique())
+        for dose in df_merge['gavage_dose_200ul'].unique():
+            print(dose)
+            df_dose = df_merge.loc[df_merge['gavage_dose_200ul']==dose]
+            #get gavage reads dataframe
+            #drop files without sequencing data
+            df_merge.dropna(subset=['file_name'], inplace = True)
+            df_gavage = df_dose[df_dose['sample_type'] == 'Gavage DNA'].copy()
+            print(df_gavage)
+            df_reads_gavage = pd.DataFrame()
+            for idx, row in df_gavage.iterrows():
+                file_path = basepath / row['ill'] / 'readtables' / row['file_name'] / f"{row['file_name']}_total_moleculestable.csv"
+                df = pd.read_csv(file_path)            
+                df['sample'] = row['project']+'_'+row['file_name']+'_'+row['sample_type']
+                df_reads_gavage = pd.concat([df_reads_gavage,df])
+
+            #get sample reads dataframe
+            #get all samples into dataframe   
+            df_samples = df_dose[df_dose['sample_type'] != 'Gavage DNA'].copy()
+            #print(df_samples)
+            df_sample_reads = pd.DataFrame()
+            for idx, row in df_samples.iterrows():
+                file_path = basepath / row['ill'] / 'readtables' / row['file_name'] / f"{row['file_name']}_total_moleculestable.csv"
+                df = pd.read_csv(file_path)
+                df['sample'] = row['project']+'_'+row['file_name']+'_'+row['sample_type']
+                df_sample_reads = pd.concat([df_sample_reads,df])
+
+            #pivot all dataframes
+            df_sample_pivot = df_sample_reads.pivot(index = ['strain', 'umi_seq'], columns = 'sample', values = 'molecules').fillna(0)
+            df_gavage_pivot = df_reads_gavage.pivot(index = ['strain', 'umi_seq'], columns = 'sample', values = 'molecules').fillna(0)
+
+            df_readstable = pd.concat([df_gavage_pivot, df_sample_pivot, df_ntc_pivot], axis = 1).fillna(0)
+
+            df_readstable.columns = df_readstable.columns.str.replace(' ', '-')
+            df_readstable.reset_index(inplace = True)
+
+            for strain in li_strains:
+                df_readstable.loc[df_readstable['strain'] == strain].drop('strain', axis = 1).to_csv(exportpath / f"{dose}_{strain}_moleculestable.csv", index = False)
+
+
+
+    except Exception as e:
+        print('Error while creating readtable')
+        import traceback
+        traceback.print_exc()
+    ### P4C3T2
+    print(50* '=')
+    print(' Creating moleculetables for P4C3T2 10K barcodese')
+    print(50*'-')
+    print(' ')
+    
+    basepath = Path(args.basepath)
+    exportpath = Path(args.output) / 'P4C3T2/moleculetables'
+    exportpath.mkdir(parents=True, exist_ok=True)
+
+    ## Read metadata & ill sheets
+    metadir = Path(args.metadir)
+    #P4C2T4:
+    df_ill = pd.read_excel(metadir / 'ill_sheets' / 'ILL138_GENEWIZ.xls',
+                                sheet_name = 'Individual Library Information', header = 1)
+    df_samplesheet = pd.read_csv(metadir / 'metadata' / 'P4C3T2_cr_mouse_sample.tsv', sep = '\t')
+
+    df_samplesheet = df_samplesheet.loc[df_samplesheet['sample_type'].str.contains('DNA')].copy()
+    df_ill = df_ill.dropna(subset=['Sample Name/Pool Name*'])
+    df_ill['sample_id']= df_ill['Sample Name/Pool Name*'].apply(lambda x: str(x).split('_')[0])
+    df_ill['project'] = df_ill['Library Name*'].apply(lambda x: str(x).split('_')[1])
+    #correct library name in ILL138 run
+    df_ill['ill'] = df_ill['Library Name*'].apply(lambda x:  'ILL138' if str(x).split('_')[0] == 'ILL139' else str(x).split('_')[0])
+    df_ill['file_name'] = df_ill['Sample Name/Pool Name*'].apply(lambda x: str(x).replace('_', '-'))
+    df_merge = pd.merge(df_ill[['sample_id', 'ill', 'file_name', 'project']], df_samplesheet, how = 'right', on = ['sample_id', 'project'])
+    df_merge.head(10)
+
+    df_ill_ntc = df_ill.loc[df_ill['Sample Name/Pool Name*'].str.contains("WATER|NTC")]
+    df_merge = df_merge.dropna(subset= ['file_name'])
+    print(df_merge)
+    print(df_ill_ntc)
+
+    
+    li_strains = ['ST1', 'ST2']
+    df_ntc = pd.DataFrame()
+    for idx, row in df_ill_ntc.iterrows():
+        file_path = basepath / row['ill'] / 'readtables' / row['file_name'] / f"{row['file_name']}_total_moleculestable.csv"
+        df = pd.read_csv(file_path)
+        df['sample'] = row['Sample Name/Pool Name*']+'_'+row['sample_id']
+        df_ntc = pd.concat([df_ntc, df])
+    
+    df_ntc_pivot = df_ntc.pivot(index = ['strain', 'umi_seq'], columns = 'sample', values = 'molecules').fillna(0)
+    print('FOUND GAVAGE DOSES:')
+    print(df_merge['gavage_dose_200ul'].unique())
+    for dose in df_merge['gavage_dose_200ul'].unique():
+        print(dose)
+        df_dose = df_merge.loc[df_merge['gavage_dose_200ul']==dose]
+        #get gavage reads dataframe
+        #drop files without sequencing data
+        df_merge.dropna(subset=['file_name'], inplace = True)
+        df_gavage = df_dose[df_dose['sample_type'] == 'Gavage DNA'].copy()
+        print(df_gavage)
+        df_reads_gavage = pd.DataFrame()
+        for idx, row in df_gavage.iterrows():
+            file_path = basepath / row['ill'] / 'readtables' / row['file_name'] / f"{row['file_name']}_total_moleculestable.csv"
+            df = pd.read_csv(file_path)            
+            df['sample'] = row['project']+'_'+row['file_name']+'_'+row['sample_type']
+            df_reads_gavage = pd.concat([df_reads_gavage,df])
+
+        #get sample reads dataframe
+        #get all samples into dataframe   
+        df_samples = df_dose[df_dose['sample_type'] != 'Gavage DNA'].copy()
+        #print(df_samples)
+        df_sample_reads = pd.DataFrame()
+        for idx, row in df_samples.iterrows():
+            file_path = basepath / row['ill'] / 'readtables' / row['file_name'] / f"{row['file_name']}_total_moleculestable.csv"
+            df = pd.read_csv(file_path)
+            df['sample'] = row['project']+'_'+row['file_name']+'_'+row['sample_type']
+            df_sample_reads = pd.concat([df_sample_reads,df])
+
+        #pivot all dataframes
+        df_sample_pivot = df_sample_reads.pivot(index = ['strain', 'umi_seq'], columns = 'sample', values = 'molecules').fillna(0)
+        df_gavage_pivot = df_reads_gavage.pivot(index = ['strain', 'umi_seq'], columns = 'sample', values = 'molecules').fillna(0)
+
+        df_readstable = pd.concat([df_gavage_pivot, df_sample_pivot, df_ntc_pivot], axis = 1).fillna(0)
+
+        df_readstable.columns = df_readstable.columns.str.replace(' ', '-')
+        df_readstable.reset_index(inplace = True)
+
+        for strain in li_strains:
+            df_readstable.loc[df_readstable['strain'] == strain].drop('strain', axis = 1).to_csv(exportpath / f"{dose}_{strain}_moleculestable.csv", index = False)
+
+
+
+    
     ### P4C1T8
     print('Creating Moleculetables for P4C1T8')
     basepath = Path(args.basepath)
@@ -260,6 +433,69 @@ def main():
         df_readstable.loc[df_readstable['strain'] == strain].drop('strain', axis = 1).to_csv(output_file, index = False)
 
 
+        ### P4C4T1
+    print('Creating Moleculetabel for P4C4T1 In Vitro')
+    basepath = Path(args.basepath)
+    exportpath = Path(args.output) / 'P4C4T1/moleculetables'
+    exportpath.mkdir(parents=True, exist_ok=True)
+    metadir = Path(args.metadir)
+    df_ill = pd.read_excel(metadir / 'ill_sheets' / 'ILL132_P4C4T1.xlsx',
+                            sheet_name = 'Individual Library Information', header = 1)
+
+    df_samplesheet = pd.read_csv(metadir / 'metadata' / 'P4C4T1_metadata.tsv', sep = '\t')
+    df_samplesheet['project'] = 'P4C4T1'
+    df_ill = df_ill.dropna(subset=['Sample Name/Pool Name*'])
+    df_ill['sample_id']= df_ill['Sample Name/Pool Name*'].apply(lambda x: str(x).split('_')[0])
+    df_ill['project'] = df_ill['Library Name*'].apply(lambda x: str(x).split('_')[1])
+    #correct library name in ILL138 run
+    df_ill['ill'] = df_ill['Library Name*'].apply(lambda x:  'ILL138' if str(x).split('_')[0] == 'ILL139' else str(x).split('_')[0])
+    df_ill['file_name'] = df_ill['Sample Name/Pool Name*'].apply(lambda x: str(x).replace('_', '-'))
+    df_merge = pd.merge(df_ill[['sample_id', 'ill', 'file_name', 'project']], df_samplesheet, how = 'right', on = ['sample_id', 'project'])
+    df_merge.head(10)
+
+    df_ill_ntc = df_ill.loc[df_ill['Sample Name/Pool Name*'].str.contains("WATER|NTC")]
+    df_merge = df_merge.dropna(subset= ['file_name'])
+    print(df_merge)
+    print(df_ill_ntc)
+
+    # generate readtables for getFP
+    li_strains = ['ST1', 'ST2', 'ST4', 'ST5', 'ST6']
+    #get gavage reads dataframe
+    #drop files without sequencing data
+    df_merge.dropna(subset=['file_name'], inplace = True)
+
+    #print(df_samples)
+    df_sample_reads = pd.DataFrame()
+    for idx, row in df_merge.iterrows():
+        df = pd.read_csv(basepath /  row['ill'] / 'readtables' /  row['file_name'] / f"{row['file_name']}_total_moleculestable.csv")
+        df['sample'] = row['project']+'_'+row['file_name']
+        df_sample_reads = pd.concat([df_sample_reads,df])
+
+    #pivot all dataframes
+    df_sample_pivot = df_sample_reads.pivot(index = ['strain', 'umi_seq'], columns = 'sample', values = 'molecules').fillna(0)
+    df_readstable = df_sample_pivot.copy()
+    df_readstable.columns = df_readstable.columns.str.replace(' ', '-')
+    df_readstable.reset_index(inplace = True)
+    for strain in li_strains:
+        df_readstable.loc[df_readstable['strain'] == strain].drop('strain', axis = 1).to_csv(exportpath / f"{strain}_moleculestable.csv", index = False)
+
+    # read all readtables, add gavage columns and drop rows in which all gavage columns are 0
+    print('Adding gavage from P4C2T4T5')
+    li_strains = ['ST1', 'ST2', 'ST4', 'ST5', 'ST6']
+    gavagepath = Path(args.output) / 'P4C2T4T5/moleculetables'
+    for strain in li_strains:
+        df_gavage = pd.read_csv(gavagepath / f"{strain}_moleculestable.csv")
+        #get gavage columns
+        li_gavage = [col for col in df_gavage.columns if 'gavage' in col.lower()]
+        print(df_gavage[li_gavage])
+        df = pd.read_csv(exportpath / f"{strain}_moleculestable.csv")
+        df.set_index('umi_seq', inplace = True)
+        df_gavage.set_index('umi_seq', inplace = True)
+        df = pd.concat([df_gavage[li_gavage] ,df], axis = 1)
+        df.reset_index(inplace = True)
+        df.fillna(0, inplace = True)
+        #drop rows in which all gavage columns are 0
+        df[df[li_gavage].sum(axis = 1) > 0].to_csv(exportpath / f"{strain}_moleculestable.csv", index = False)
         
 if __name__ == "__main__":
     main()
